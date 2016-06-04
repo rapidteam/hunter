27c305f878f946fa3d14f78c5d16d9883b3ab093
rapidteam <rapidtele@gmail.com>
2016-06-04 21:54:57 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (hunter)",-- Put everything you like :)
+		"^[!/]([Hh]unter)"-- Put everything you like :)
     }, 
   run = run
 }
